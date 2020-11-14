ebb528559e5ec46ca3f06ba01c88c10ad6924ee1
Julio <59529624+Mega646@users.noreply.github.com>
2020-11-14 18:02:26 +0100

    Attempt at creating the "playlist" functionality
    The idea is that, upon scanning the folder, the properties of the script should be updated, adding more properties to it, one for each song in the scanned folder, that allows to set an order of reproduction for each song. However, I can't for the life of me add these new properties. Here I try to re-enter the script_update function since it's the only place where properties can be added seemingly, but apparently that only works on program startup. So, yeah. Completely blocked here.

--- music-handler.py
+++ music-handler.py
@@ -5,6 +5,9 @@ text_source_name = ""
 song_directories = ""
 target_scene = ""
 group_name = ""
+create_playlist = False
+song_number = 0
+props = None
 
 def update_text():
     song = get_active_song()
@@ -27,14 +30,21 @@ def get_active_song():
             active = obs.obs_source_active(source)
             if source_id == "ffmpeg_source" and active is True:
                 return source
+
+def create_playlist(props, song_number):
+    for i in range(1, song_number):
+        obs.obs_properties_add_int(props, "priority" + str(i), "Priority of song: " + str(i), 0, 100, 0)
+        print(i)
             
 def scan_folder():
     global target_scene
     global group_name
+    song_number = 0
     scene = scene_name_to_scene(target_scene)
     group = obs.obs_scene_find_source(scene, group_name)
     if group is not None:
         obs.obs_sceneitem_remove(group)
+        obs.obs_sceneitem_release(group)
     group = obs.obs_scene_add_group(scene, group_name)
     for file in os.listdir(song_directories):
         if file.endswith((".mp3", ".webm", ".m4a", ".ogg")):
@@ -45,26 +55,36 @@ def scan_folder():
             scene_item = obs.obs_scene_add(scene, source)
             group = obs.obs_scene_get_group(scene, group_name)
             obs.obs_sceneitem_group_add_item(group, scene_item)
+            obs.obs_data_release(settings)
+            obs.obs_source_release(source)
+            song_number = song_number + 1
+    group_source = obs.obs_sceneitem_get_source(group)
+    return song_number
 
 def obs_source_print_json(source):
     settings = obs.obs_source_get_settings(source)
     json = obs.obs_data_get_json(settings)
     print(json)
 
-def scene_name_to_scene(scene):
-    source = obs.obs_get_source_by_name(scene)
+def scene_name_to_scene(scene_name):
+    source = obs.obs_get_source_by_name(scene_name)
     if source is not None:
         scene = obs.obs_scene_from_source(source)
         obs.obs_source_release(source)
         return scene
     else:
+        obs.obs_source_release(source)
         return None
             
 def signal_receiver(cd):
     update_text()
 
-def button_handler(lm, ao):
-    scan_folder()
+def button_handler(props, prop):
+    global song_number
+    song_number = scan_folder()
+    global create_playlist
+    create_playlist = True
+    script_properties()
             
 #----------------------------------------------------------------
 
@@ -72,21 +92,30 @@ def script_description():
     return "Song manager that allows you to scan folders and add all the music in there as sources in a group. It also lets you display the name of the source for credit. Made by Mega64."
 
 def script_properties():
-    props = obs.obs_properties_create()
-    source_list = obs.obs_properties_add_list(props, "text_source_list", "Source List", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
-    sources = obs.obs_enum_sources()
-    if sources is not None:
-        for source in sources:
-            source_id = obs.obs_source_get_unversioned_id(source)
-            if source_id == "text_gdiplus" or source_id == "text_ft2_source":
-                source_name = obs.obs_source_get_name(source)
-                obs.obs_property_list_add_string(source_list, source_name, source_name)
-    obs.obs_properties_add_path(props, "path", "List of reproduction folder", obs.OBS_PATH_DIRECTORY, None, None)
-    obs.obs_properties_add_text(props, "scene", "Target scene", obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_text(props, "group", "Target group name", obs.OBS_TEXT_DEFAULT)
-    obs.obs_properties_add_button(props, "refresh", "Refresh folder", button_handler)
-    obs.source_list_release(sources)
-    return props
+    global create_playlist
+    if create_playlist is False:
+        global props
+        props = obs.obs_properties_create()
+        source_list = obs.obs_properties_add_list(props, "text_source_list", "Source List", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+        sources = obs.obs_enum_sources()
+        if sources is not None:
+            for source in sources:
+                source_id = obs.obs_source_get_unversioned_id(source)
+                if source_id == "text_gdiplus" or source_id == "text_ft2_source":
+                    source_name = obs.obs_source_get_name(source)
+                    obs.obs_property_list_add_string(source_list, source_name, source_name)
+        obs.obs_properties_add_path(props, "path", "List of reproduction folder", obs.OBS_PATH_DIRECTORY, None, None)
+        obs.obs_properties_add_text(props, "scene", "Target scene", obs.OBS_TEXT_DEFAULT)
+        obs.obs_properties_add_text(props, "group", "Target group name", obs.OBS_TEXT_DEFAULT)
+        obs.obs_properties_add_button(props, "refresh", "Refresh folder", button_handler)
+        obs.source_list_release(sources)
+        return props
+    else:
+        global song_number
+        for i in range(1, song_number):
+            obs.obs_properties_add_int(props, "priority" + str(i), "Priority of song: " + str(i), 0, 100, 0)
+            print("¿Hola?")
+        return props
 
 def script_update(settings):
     global song_directories
@@ -112,7 +141,10 @@ def script_unload():
     
 
 def script_load(settings):
+    global create_playlist
+    create_playlist = False
     signal_handler = obs.obs_get_signal_handler()
     obs.signal_handler_connect(signal_handler, "source_activate", signal_receiver)
     obs.signal_handler_connect(signal_handler, "source_deactivate", signal_receiver)
     update_text()
+    
